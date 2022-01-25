1e4ac7f1540df9245a20fc2002dfb39e1589d353
baudoinalkali <79876365+baudoinalkali@users.noreply.github.com>
2022-01-24 21:54:57 -0600

    header and footer menu

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,51 @@
+import Image from "next/image"
 export default function Footer() {
   return (
-    <footer id="footer">
-    <h1>Footer</h1>
+    <footer id="footer" className="gradient-bg py-14">
+      <div className="flex max-w-7xl mx-auto w-11/12">
+        <div>
+          <Image
+            src="/images/logo-footer.svg"
+            width={140}
+            height={88}
+          />
+          <p className="max-w-lg mt-6 text-white">
+            I'm baby portland sustainable street art flannel next level four loko leggings DIY cray hoodie crucifix
+            unicorn offal gluten-free. IPhone blog air plant, adaptogen narwhal waistcoat man braid prism deep v mustache
+          </p>
+        </div>
+      </div>
+      <div className="flex max-w-7xl mx-auto w-11/12 mt-10 items-center justify-between">
+        <p className="text-white">
+          Copyright Notice
+        </p>
+        <div className="flex items-center gap-x-3">
+          <Image
+            className="rounded-2xl"
+            src="/images/facebook.svg"
+            width={40}
+            height={40}
+          />
+          <Image
+            className="rounded-2xl"
+            src="/images/instagram.svg"
+            width={40}
+            height={40}
+          />
+          <Image
+            className="rounded-2xl"
+            src="/images/twitter.svg"
+            width={40}
+            height={40}
+          />
+          <Image
+            className="rounded-2xl"
+            src="/images/linkedin.svg"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
     </footer>
   )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react'
 import Button from './button'
 import Logo from './logo'
+import Image from "next/image"
+import MobileDropdown from './mobileDropdown'
 export default function Header(props) {
-    const [ color, setColor ] = useState(props.defaultColor)
-    const [ backgroundColor, setBackgroundColor ] = useState(props.defaultBGColor)
+    const [color, setColor] = useState(props.defaultColor)
+    const [backgroundColor, setBackgroundColor] = useState(props.defaultBGColor)
     useEffect(() => {
         window.addEventListener('scroll', handleScroll)
         return () => window.removeEventListener('scroll', handleScroll)
     })
 
     const handleScroll = () => {
-        if ( window.scrollY > 50 ) {
+        if (window.scrollY > 50) {
             setBackgroundColor(props.scrollBGColor)
             setColor(props.scrollColor)
         } else {
@@ -19,19 +21,34 @@ export default function Header(props) {
         }
     }
     return (
-        <div style={{backgroundColor: backgroundColor}} className="fixed w-full text-xl font-bold flex py-4 top-0 z-10">
-            <div className="px-8 flex">
-                <Logo fill={color} />
+        <div style={{ backgroundColor: backgroundColor }} className='fixed w-full flex py-4 top-0 z-10'>
+            <div className="hidden lg:flex w-11/12 mx-auto justify-between" style={{ maxWidth: "80rem" }}>
+                <div className="flex">
+                    <Logo fill={color} />
+                </div>
+                <div className="grow self-center flex font-bold justify-evenly text-xl">
+
+                </div>
+                <div className="flex items-center space-x-10">
+                    <ul className="flex space-x-5 text-white text-lg open font-bold">
+                        <li>Button</li>
+                        <li>Button</li>
+                        <li>Button</li>
+                        <li>Button</li>
+                        <li>Button</li>
+                    </ul>
+                    <Button text="Call to action" color="primary" styling="px-10" />
+                </div>
             </div>
-            <div className="grow self-center flex justify-evenly">
-                <a href="#" style={{color}}>Button</a>
-                <a href="#" style={{color}}>Button</a>
-                <a href="#" style={{color}}>Button</a>
-                <a href="#" style={{color}}>Button</a>
-                <a href="#" style={{color}}>Button</a>
-            </div>
-            <div className="px-8 self-center">
-                <Button text="Call to action" color="primary" />
+            <div className="flex lg:hidden items-center justify-between max-w-7xl mx-auto w-11/12">
+                <Image 
+                    src="/images/logo-white.svg"
+                    width={215}
+                    height={47}
+                />
+                <div className="flex justify-center">
+                <MobileDropdown />
+                </div>
             </div>
         </div>
     )
